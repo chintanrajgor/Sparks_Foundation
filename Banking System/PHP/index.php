8708a8d5824d61bc49ffaae571217a6ac69c40c1
ChintanRajgor <61462766+chintanrajgor@users.noreply.github.com>
2021-05-18 16:03:21 +0530

    Add files via upload

---
+++ Banking System/PHP/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Basic Banking System, By- Chintan Rajgor</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/1111.css" type="text/css">
+    <link rel="stylesheet" href="css/3333.css" type="text/css">
+    <link rel="stylesheet" href="css/222.css" type="text/css">
+    <link rel="stylesheet" type="text/css" href="css/444.css">
+
+</head>
+<body  style="color:blue; background-color:lightgrey ">
+
+    <?php
+    include 'navbar.php';
+    ?>
+
+    <div class="toppane my-3">
+        <div class="row intro py-1" style="background-color: lightgrey;">
+            <div class="xyz">
+               
+                 <div class="container">
+            <div class="leftpane" style="background-color: lightgrey">
+                <img src="img/2.png" alt="userimg" class="img-fluid">
+                
+            </div>
+
+            <div class="rightpane">
+                 <div class="heading text-center my-5 ">
+                    <h1>Welcome to<br>Banking System</h1>
+                </div>
+                <br>
+                 <a href="createuser.php"><button class="button my-2"style="background-color: #black;">Create User</button></a>
+               
+                <br>
+               
+                <a href="transfermoney.php"><button class="button my-2"style="background-color: #black;">Make Transaction</button></a>
+                <br>
+                <a href="transactionhistory.php"><button class="button my-2"style="background-color: #black;">Transaction History</button></a>
+            </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+
+</body>
+
+
+<footer class="footer" style="color: white; background-color:black ">
+        <p>&copy 2021. Made By <b>Chintan Rajgor</b></p>
+    </footer>
+</html>
